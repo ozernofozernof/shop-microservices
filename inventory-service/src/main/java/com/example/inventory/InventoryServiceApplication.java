a97b83908d80b916000f2fd3025951ac8e008846
Pavel Kuznetsov <ozernof@gmail.com>
2025-11-21 11:09:00 +0300

    Добавил proto, docker, main'ы и application

--- inventory-service/src/main/java/com/example/inventory/InventoryServiceApplication.java
+++ inventory-service/src/main/java/com/example/inventory/InventoryServiceApplication.java
@@ -1,11 +1,11 @@
-package com.example.notification;
+package com.example.inventory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NotificationServiceApplication {
+public class InventoryServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(NotificationServiceApplication.class, args);
+        SpringApplication.run(InventoryServiceApplication.class, args);
     }
 }
--- order-service/src/main/java/com/example/order/OrderServiceApplication.java
+++ order-service/src/main/java/com/example/order/OrderServiceApplication.java
@@ -1,11 +1,11 @@
-package com.example.notification;
+package com.example.order;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NotificationServiceApplication {
+public class OrderServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(NotificationServiceApplication.class, args);
+        SpringApplication.run(OrderServiceApplication.class, args);
     }
 }
